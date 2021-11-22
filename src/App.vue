6ffd75689000e794690dc4e378b4dc4837cafd10
Hanasagi <37210818+Hanasagi@users.noreply.github.com>
2021-11-22 12:23:21 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
